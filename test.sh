e874b3a70eac2c7c07158cdea81d88e791e057fa
Bo Wee <sekbowee@gmail.com>
2021-05-08 11:50:51 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd .. || exit 5
 
-if [ ! -d "Prusa-Firmware-test" ]; then
-    mkdir Prusa-Firmware-test  || exit 10
+if [ ! -d "3DXFAB-Firmware-test" ]; then
+    mkdir 3DXFAB-Firmware-test  || exit 10
 fi
 
-cd Prusa-Firmware-test || exit 20
+cd 3DXFAB-Firmware-test || exit 20
 
-cmake -G "Eclipse CDT4 - Ninja" ../Prusa-Firmware || exit 30
+cmake -G "Eclipse CDT4 - Ninja" ../3DXFAB-Firmware || exit 30
 cmake --build . || exit 35
 ./tests || exit 40
